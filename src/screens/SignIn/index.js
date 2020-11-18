b447b056f1278378f07aca82840d93e6dabcddc9
vargattj <vargattjh@gmail.com>
2020-11-18 10:18:04 -0300

    Tela de SignIn

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import {Text} from 'react-native';
-import {Container} from './styles';
+import {
+  Container,
+  InputArea,
+  CustomButton,
+  CustomButtonText,
+  SignMessageButton,
+  SignMessageButtonText,
+  SignMessageButtonTextBold,
+} from './styles';
+
+import SignInput from '../../components/SignInput';
+import BarberLogo from '../../assets/barber.svg';
+import EmailIcon from '../../assets/email.svg';
+import LockIcon from '../../assets/lock.svg';
 
 export default () => {
   return (
     <Container>
-      <Text>SignIn</Text>
+      <BarberLogo width="100%" height="160" />
+      <InputArea>
+        <SignInput IconSvg={EmailIcon} placeholder="Digite Seu e-mail" />
+        <SignInput IconSvg={LockIcon} placeholder="Digite sua Senha" />
+
+        <CustomButton>
+          <CustomButtonText>Login</CustomButtonText>
+        </CustomButton>
+      </InputArea>
+
+      <SignMessageButton>
+        <SignMessageButtonText>Ainda não possui conta?</SignMessageButtonText>
+        <SignMessageButtonTextBold>Cadastre-se</SignMessageButtonTextBold>
+      </SignMessageButton>
     </Container>
   );
 };
