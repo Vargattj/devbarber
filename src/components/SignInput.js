69075e2e1b1a18af6ddccddd97e08dad36573fd6
vargattj <vargattjh@gmail.com>
2020-11-20 10:54:23 -0300

    home page pronta

--- src/components/SignInput.js
+++ src/components/SignInput.js
@@ -1,5 +1,23 @@
 import React from 'react';
-import {InputArea, Input} from './styles';
+import styled from 'styled-components/native';
+
+const InputArea = styled.View`
+  width: 100%;
+  height: 60px;
+  background-color: #83d6e3;
+  flex-direction: row;
+  border-radius: 30px;
+  padding-left: 20px;
+  align-items: center;
+  margin-bottom: 15px;
+`;
+
+const Input = styled.TextInput`
+  flex: 1;
+  font-size: 16px;
+  color: #268596;
+  margin-left: 10px;
+`;
 
 export default ({IconSvg, placeholder, value, onChangeText, password}) => {
   return (
--- src/reducers/UserReducer.js
+++ src/reducers/UserReducer.js
@@ -7,7 +7,6 @@ export const initialState = {
 export const UserReducer = (state, action) => {
   switch (action.type) {
     case 'setAvatar':
-      console.log(state, '||||', action);
       return {...state, avatar: action.payload.avatar};
       break;
     default:
